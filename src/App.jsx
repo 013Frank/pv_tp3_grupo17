0b682babe237537f24ffa969e208ccfb7cc8e9db
siomaraven <siomaravenicio@gmail.com>
2025-05-20 21:13:19 -0300

    Actulizamos TaskList para que sea independiente de TaskInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React, { useState } from 'react'
 import './App.css'
 import TaskInput from '../src/components/TaskInput'
+import TaskList from '../src/components/TaskList'
+
 function App() {
+  const [list, setlist] = useState([]) ;
+
   return (
-    <TaskInput />
-    
-  )
+    <div>
+      <TaskInput list={list} setlist={setlist} /> 
+      {list.length > 0 ? (
+        <TaskList list={list} setlist={setlist} />
+      ) : (
+        <p>No hay tareas</p>
+      )}
+    </div>
+  );
 }
 
 export default App
